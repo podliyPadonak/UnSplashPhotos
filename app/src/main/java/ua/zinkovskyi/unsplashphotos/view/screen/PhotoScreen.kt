61e27a4eadcd170e402c6a80443c4999e62aefee
ivan.zinkovskyi <ivan.zinkovskyi@hebronsoft.com>
2025-03-20 20:30:51 +0200

    better ui

--- app/src/main/java/ua/zinkovskyi/unsplashphotos/view/screen/PhotoScreen.kt
+++ app/src/main/java/ua/zinkovskyi/unsplashphotos/view/screen/PhotoScreen.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.ColorScheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import ua.zinkovskyi.unsplashphotos.data.Image
 import ua.zinkovskyi.unsplashphotos.view.component.appBarText
@@ -22,6 +25,7 @@ import ua.zinkovskyi.unsplashphotos.view.component.appBarText
 fun PhotoScreen(photo: Image) {
     val imageUrl = photo.urls.full
     val imagePainter = rememberAsyncImagePainter(imageUrl)
+    val state = imagePainter.state
 
     appBarText.value = photo.user.name
 
@@ -30,6 +34,24 @@ fun PhotoScreen(photo: Image) {
             .fillMaxWidth()
             .wrapContentHeight()
     ) {
+        when (state) {
+            is AsyncImagePainter.State.Loading -> {
+                CircularProgressIndicator(Modifier.align(Alignment.CenterHorizontally))
+            }
+
+            is AsyncImagePainter.State.Success -> {
+
+            }
+
+            is AsyncImagePainter.State.Error -> {
+                Icon(
+                    Icons.Default.Close,
+                    contentDescription = "Back"
+                )
+            }
+
+            else -> {}
+        }
         Box {
             Image(
                 painter = imagePainter,
@@ -39,7 +61,7 @@ fun PhotoScreen(photo: Image) {
             )
         }
 
-        Column (
+        Column(
             Modifier.padding(8.dp)
         ) {
             Text(
